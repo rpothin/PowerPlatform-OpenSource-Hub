ed8332685af4cc0c65fbfc124185843b555840b7
Raphael Pothin <raphael.pothin@gmail.com>
2023-09-09 01:17:41 +0000

    Implementation of unit tests for the Search-GitHubRepositories script

---
+++ Scripts/Tests/Search-GitHubRepositories.Tests.ps1
@@ -0,0 +1,90 @@
+# To execute these tests run the following command from the root of the repository: Invoke-Pester -Script .\Scripts\Tests\.\Scripts\Search-GitHubRepositories.ps1.Tests.ps1
+
+# Set a global variable with the path of the executing script
+$global:executingTestPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+BeforeAll {
+    # Import Search-GitHubRepositories function
+    Import-Module $global:executingTestPath/../Search-GitHubRepositories.ps1 -Force
+}
+
+Describe "Search-GitHubRepositories Unit Tests" {
+    Context "Parameter validation" {
+        It "Should throw an error if Topic parameter contains spaces" {
+            $result = { Search-GitHubRepositories -Topic "invalid topic" -SearchLimit 10 } | Should -Throw -PassThru
+            $result.Exception.Message | Should -Be "The value of the Topic parameter cannot contain spaces."
+        }
+
+        It "Should throw an error if SearchLimit is not a positive integer" {
+            $result = { Search-GitHubRepositories -Topic "validtopic" -SearchLimit -1 } | Should -Throw -PassThru
+            $result.Exception.Message | Should -Be "Cannot validate argument on parameter 'SearchLimit'. The -1 argument is less than the minimum allowed range of 1. Supply an argument that is greater than or equal to 1 and then try the command again."
+        }
+    }
+
+    Context "Execution With a Mocked gh Command" {
+        BeforeEach {
+            Mock gh {
+                "[
+                    {
+                      'createdAt': '0000-00-00T00:00:00Z',
+                      'description': 'Anonymised Description',
+                      'fullName': 'anonymised/Anonymised',
+                      'hasIssues': true,
+                      'homepage': 'https://anonymised.url',
+                      'language': 'Anonymised Language',
+                      'license': {
+                        'key': 'anonymised',
+                        'name': 'Anonymised License',
+                        'url': 'https://anonymised.url'
+                      },
+                      'name': 'Anonymised',
+                      'openIssuesCount': 0,
+                      'owner': {
+                        'id': 'AnonymisedID',
+                        'is_bot': false,
+                        'login': 'anonymised',
+                        'type': 'Anonymised',
+                        'url': 'https://anonymised.url'
+                      },
+                      'updatedAt': '0000-00-00T00:00:00Z',
+                      'url': 'https://anonymised.url',
+                      'watchersCount': 0
+                    },
+                    {
+                        'createdAt': '0000-00-00T00:00:00Z',
+                        'description': 'Anonymised Description',
+                        'fullName': 'anonymised/Anonymised',
+                        'hasIssues': true,
+                        'homepage': 'https://anonymised.url',
+                        'language': 'Anonymised Language',
+                        'license': {
+                          'key': 'anonymised',
+                          'name': 'Anonymised License',
+                          'url': 'https://anonymised.url'
+                        },
+                        'name': 'Anonymised',
+                        'openIssuesCount': 0,
+                        'owner': {
+                          'id': 'AnonymisedID',
+                          'is_bot': false,
+                          'login': 'anonymised',
+                          'type': 'Anonymised',
+                          'url': 'https://anonymised.url'
+                        },
+                        'updatedAt': '0000-00-00T00:00:00Z',
+                        'url': 'https://anonymised.url',
+                        'watchersCount': 0
+                      }
+                  ]"
+            }
+        }
+
+        It "Should return a repository object with correct properties when valid parameters are provided" {
+            $result = Search-GitHubRepositories -Topic "validtopic" -SearchLimit 1
+            $result | Should -Not -BeNullOrEmpty
+            $result[0].fullName | Should -Be "anonymised/Anonymised"
+            $result[0].language | Should -Be "Anonymised Language"
+            $result.count | Should -Be 2
+        }
+    }
+}
