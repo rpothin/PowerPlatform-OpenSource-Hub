ad984db738eaa9653b18826ed2f6f7d678d762bd
Raphael Pothin <raphael.pothin@gmail.com>
2023-09-12 03:00:15 +0000

    Add a new script + tests to get more details for a GitHub repository

---
+++ Scripts/Get-GitHubRepositoryDetails.ps1
@@ -0,0 +1,75 @@
+function Get-GitHubRepositoryDetails {
+    <#
+        .SYNOPSIS
+            Retrieve details of a GitHub repository.
+
+        .DESCRIPTION
+            Using GitHub CLI combined with the GitHub API, this function retrieves details of a GitHub repository.
+
+        .PARAMETER RepositoryFullName
+            The full name (organization/repository) of the GitHub repository to retrieve details for.
+
+        .INPUTS
+            None. You cannot pipe objects to Get-GitHubRepositoryDetails.
+
+        .OUTPUTS
+            Object. This script returns the details of the considered GitHub repository.
+
+        .EXAMPLE
+            Import-Module .\Scripts\Get-GitHubRepositoryDetails.ps1 -Force
+            Get-GitHubRepositoryDetails -RepositoryFullName "sampleUser/sampleRepo"
+                codeOfConduct           : @{key=sample_key; name=Sample Name; url=https://github.com/sampleUser/sampleRepo/blob/Dev/CODE_OF_CONDUCT.md}
+                forkCount               : 123
+                fundingLinks            : {}
+                isSecurityPolicyEnabled : True
+                isTemplate              : False
+                languages               : {@{size=1234567; node=}, @{size=123; node=}, @{size=123; node=}, @{size=123; node=}…}
+                latestRelease           : @{name=Release 1.2.3; tagName=1.2.3; url=https://github.com/sampleUser/sampleRepo/releases/tag/1.2.3; publishedAt=01/01/2022 12:00:00}
+                primaryLanguage         : @{name=SampleLanguage}
+                securityPolicyUrl       : https://github.com/sampleUser/sampleRepo/security/policy
+                stargazerCount          : 123
+                watchers                : @{totalCount=12}
+                topics                  : {topic1, topic2, topic3, topic4…}
+    #>
+
+    [CmdletBinding()]
+    [OutputType([psobject])]
+    Param(
+        # The full name (organization/repository) of the GitHub repository to retrieve details for.
+        [Parameter(Mandatory = $true)]
+        [string]$RepositoryFullName
+    )
+
+    Process{
+        # Validate the format of the repository full name
+        if ($RepositoryFullName -notmatch "^[a-zA-Z0-9-]+/[a-zA-Z0-9-]+$") {
+            Throw "The repository full name '$RepositoryFullName' is not valid."
+        }
+
+        # Initialize the repository details object
+        $repositoryDetails = [PSCustomObject]@{}
+
+        # Validate the existence of the repository and get a first round of details
+        $repositoryDetails = gh repo view $RepositoryFullName --json codeOfConduct,forkCount,fundingLinks,isSecurityPolicyEnabled,isTemplate,languages,latestRelease,primaryLanguage,securityPolicyUrl,stargazerCount,watchers 2>&1
+
+        # If the repository does not exist, add an error message to the repository details object
+        if ($repositoryDetails -is [System.Management.Automation.ErrorRecord]) {
+            $repositoryDetails = New-Object PSObject # In that case we need to create a new object to be able to add the error message because the object returned by the GitHub CLI is not a PSObject
+
+            $errorMessage = "The repository '$RepositoryFullName' does not exist."
+            $repositoryDetails | Add-Member -MemberType NoteProperty -Name "error" -Value $errorMessage
+        } 
+        # If the repository exists, get its topics and add them to the repository details object
+        else {
+            $repositoryTopics = (gh api repos/$RepositoryFullName | ConvertFrom-Json) | Select-Object topics
+
+            # Convert the object from JSON to PSObject to be able to add the topics
+            $repositoryDetails = $repositoryDetails | ConvertFrom-Json
+
+            $repositoryDetails | Add-Member -MemberType NoteProperty -Name "topics" -Value $repositoryTopics.topics
+        }
+
+        # Return the details of the GitHub repository
+        $repositoryDetails
+    }
+}
---
+++ Scripts/Tests/Get-GitHubRepositoryDetails.Tests.ps1
@@ -0,0 +1,123 @@
+# To execute these tests run the following command from the root of the repository: Invoke-Pester -Script .\Scripts\Tests\Get-GitHubRepositoryDetails.Tests.ps1
+
+# Set a global variable with the path of the executing script
+$global:executingTestPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+BeforeAll {
+    # Import Get-GitHubRepositoryDetails function
+    Import-Module $global:executingTestPath/../Get-GitHubRepositoryDetails.ps1 -Force
+}
+
+Describe "Get-GitHubRepositoryDetails Unit Test" {
+    Context "Parameters validation" {
+        It "Should throw an error if RepositoryFullName parameter is not valid" {
+            $result = { Get-GitHubRepositoryDetails -RepositoryFullName "invalid repository full name" } | Should -Throw -PassThru
+            $result.Exception.Message | Should -Be "The repository full name 'invalid repository full name' is not valid."
+        }
+    }
+
+    Context "Valid execution for a non-existing repository" {
+        BeforeEach {
+            Mock gh {
+                $errorRecord = New-Object System.Management.Automation.ErrorRecord -ArgumentList @(
+                    [System.Exception]::new("The repository 'anon/repo' does not exist."),
+                    "RepositoryDoesNotExist",
+                    [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+                    $null
+                )
+                $errorRecord
+            }
+        }
+
+        It "Should return an error message if GitHub repository does not exist" {
+            $result = Get-GitHubRepositoryDetails -RepositoryFullName "anon/repo"
+            $result.error | Should -Be "The repository 'anon/repo' does not exist."
+        }
+    }
+
+    Context "Valid execution for an existing repository" {
+        BeforeEach {
+            Mock gh {
+                # Check the parameters that were passed to the 'gh' command
+                param($Command)
+
+                if ($Command -eq 'repo') {
+                    # If the 'gh' command was called with 'repo view', return this mock data as a multi-line JSON structure as a string
+                    return "{
+                        'codeOfConduct': {
+                            'key': 'anon_covenant',
+                            'name': 'Anon Covenant',
+                            'url': 'https://anon.com/AnonCovenant.md'
+                        },
+                        'forkCount': 0,
+                        'fundingLinks': [],
+                        'isSecurityPolicyEnabled': true,
+                        'isTemplate': false,
+                        'languages': [
+                            {
+                            'size': 0,
+                            'node': {
+                                'name': 'AnonScript'
+                            }
+                            }
+                        ],
+                        'latestRelease': {
+                            'name': 'Release 1.0.0',
+                            'tagName': '1.0.0',
+                            'url': 'https://anon.com/anon/anon/releases/tag/1.0.0',
+                            'publishedAt': '2000-01-01T00:00:00Z'
+                        },
+                        'primaryLanguage': {
+                            'name': 'AnonScript'
+                        },
+                        'repositoryTopics': [
+                            {
+                            'name': 'anon'
+                            }
+                        ],
+                        'securityPolicyUrl': 'https://anon.com/anon/anon/security/policy',
+                        'stargazerCount': 0,
+                        'watchers': {
+                            'totalCount': 0
+                        }
+                    }"
+                }
+                elseif ($Command -eq 'api') {
+                    # If the 'gh' command was called with 'api repos', return this mock data
+                    return "{
+                        'topics': [
+                            'topic1',
+                            'topic2',
+                            'topic3',
+                            'topic4'
+                        ]
+                    }"
+                }
+            }
+        }
+
+        It "Should return the details of the considered GitHub repository if it exists" {
+            $result = Get-GitHubRepositoryDetails -RepositoryFullName "anon/repo"
+            $result.codeOfConduct.key | Should -Be "anon_covenant"
+            $result.codeOfConduct.name | Should -Be "Anon Covenant"
+            $result.codeOfConduct.url | Should -Be "https://anon.com/AnonCovenant.md"
+            $result.forkCount | Should -Be 0
+            $result.fundingLinks | Should -Be @()
+            $result.isSecurityPolicyEnabled | Should -Be $true
+            $result.isTemplate | Should -Be $false
+            $result.languages[0].size | Should -Be 0
+            $result.languages[0].node.name | Should -Be "AnonScript"
+            $result.latestRelease.name | Should -Be "Release 1.0.0"
+            $result.latestRelease.tagName | Should -Be "1.0.0"
+            $result.latestRelease.url | Should -Be "https://anon.com/anon/anon/releases/tag/1.0.0"
+            $result.latestRelease.publishedAt | Should -Be "01/01/2000 00:00:00"
+            $result.primaryLanguage.name | Should -Be "AnonScript"
+            $result.repositoryTopics[0].name | Should -Be "anon"
+            $result.securityPolicyUrl | Should -Be "https://anon.com/anon/anon/security/policy"
+            $result.stargazerCount | Should -Be 0
+            $result.watchers.totalCount | Should -Be 0
+            $result.topics[0] | Should -Be "topic1"
+            $result.topics[1] | Should -Be "topic2"
+        }
+    }
+}
