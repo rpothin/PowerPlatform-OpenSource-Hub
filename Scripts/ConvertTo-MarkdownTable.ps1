2fc20580b3d66763b60a4aee5f884005a0e18116
Raphael Pothin <raphael.pothin@gmail.com>
2023-09-13 02:45:21 +0000

    Script to convert objects into a markdown table

---
+++ Scripts/ConvertTo-MarkdownTable.ps1
@@ -0,0 +1,64 @@
+function ConvertTo-MarkdownTable {
+    <#
+        .SYNOPSIS
+            Convert the content of an object as a markdown table.
+
+        .DESCRIPTION
+            Create a markdown table from the content of an object as an array where each row is a line of the table.
+            The properties names are used as the headers of the table and the values as the rows.
+
+        .PARAMETER InputObject
+            The object to convert to a markdown table.
+
+        .INPUTS
+            This function accepts an object as an input.
+
+        .OUTPUTS
+            String. This function returns a string containing the markdown table.
+
+        .EXAMPLE
+            Import-Module .\Scripts\ConvertTo-MarkdownTable.ps1 -Force
+            $object | ConvertTo-MarkdownTable
+                | Property1 | Property2 | Property3 |
+                | --------- | --------- | --------- |
+                | Value1    | Value2    | Value3    |
+                | Value1    | Value2    | Value3    |
+    #>
+
+    [CmdletBinding()]
+    [OutputType([string])]
+    Param(
+        # The object to convert to a markdown table.
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [psobject]$InputObject
+    )
+
+    Process {
+        # Check that the input object is of type psobject
+        if ($InputObject.GetType().Name -ne 'PSCustomObject') {
+            throw "The input object is not of type 'PSCustomObject'."
+        }
+
+        # Check that the input object is not empty
+        $inputObjectAsJson = $InputObject | ConvertTo-Json
+        if ($inputObjectAsJson -eq "{}" -or $inputObjectAsJson -eq "[]") {
+            throw "The input object is empty."
+        }
+
+        # Initialize variables
+        $headersDone = $false
+        $pattern = '(?<!\\)\|'  # escape every '|' unless already escaped
+
+        # If the headers are not configured yet, configure them
+        if (!$headersDone) {
+            $headersDone = $true
+            
+            # Configure the headers
+            '|{0}|' -f (($_.PSObject.Properties.Name -replace $pattern, '\|') -join '|')
+            '|{0}|' -f (($_.PSObject.Properties.Name -replace '.', '-') -join '|')
+        }
+
+        # Configure the rows
+        '|{0}|' -f (($_.PsObject.Properties.Value -replace $pattern, '\|') -join '|')
+    }
+}
---
+++ Scripts/Tests/ConvertTo-MarkdownTable.Tests.ps1
@@ -0,0 +1,58 @@
+# To execute these tests run the following command from the root of the repository: Invoke-Pester -Script .\Scripts\Tests\ConvertTo-MarkdownTable.Tests.ps1
+
+# Set a global variable with the path of the executing script
+$global:executingTestPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+BeforeAll {
+    # Import ConvertTo-MarkdownTable function
+    Import-Module $global:executingTestPath/../ConvertTo-MarkdownTable.ps1 -Force
+}
+
+Describe "ConvertTo-MarkdownTable Unit Tests" {
+    Context "Parameters validation" {
+        It "Should throw an error if InputObject parameter is not valid" {
+            $inputObject = "invalid object"
+            $result = { $inputObject | ConvertTo-MarkdownTable } | Should -Throw -PassThru
+            $result.Exception.Message | Should -Be "The input object is not of type 'PSCustomObject'."
+        }
+
+        It "Should throw an error if InputObject parameter is empty" {
+            $emptyObject = New-Object PSObject
+            { $emptyObject | ConvertTo-MarkdownTable } | Should -Throw "The input object is empty."
+        }
+    }
+
+    Context "Valid execution" {
+        It "Should return markdown table with headers and 1 row for 1 object" {
+            $inputObject = [PSCustomObject]@{
+                Property1 = "Value1"
+                Property2 = "Value2"
+                Property3 = "Value3"
+            }
+            $result = $inputObject | ConvertTo-MarkdownTable
+            $result | Should -Be @('|Property1|Property2|Property3|', '|---------|---------|---------|', '|Value1|Value2|Value3|')
+        }
+
+        It "Should return markdown table with headers and 3 row2 for an array of 3 objects" {
+            $inputObject = @(
+                [PSCustomObject]@{
+                    Property1 = "Value1"
+                    Property2 = "Value2"
+                    Property3 = "Value3"
+                },
+                [PSCustomObject]@{
+                    Property1 = "Value1"
+                    Property2 = "Value2"
+                    Property3 = "Value3"
+                },
+                [PSCustomObject]@{
+                    Property1 = "Value1"
+                    Property2 = "Value2"
+                    Property3 = "Value3"
+                }
+            )
+            $result = $inputObject | ConvertTo-MarkdownTable
+            $result | Should -Be @('|Property1|Property2|Property3|', '|---------|---------|---------|', '|Value1|Value2|Value3|', '|Property1|Property2|Property3|', '|---------|---------|---------|', '|Value1|Value2|Value3|', '|Property1|Property2|Property3|', '|---------|---------|---------|', '|Value1|Value2|Value3|')
+        }
+    }
+}
