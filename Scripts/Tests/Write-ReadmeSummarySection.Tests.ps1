7e6d21723fa9cc83e87c35de8d878eb892310711
Raphael Pothin <raphael.pothin@gmail.com>
2023-09-19 02:51:26 +0000

    Initialize Write-ReadmeSummarySection script

---
+++ Scripts/Tests/Write-ReadmeSummarySection.Tests.ps1
@@ -0,0 +1,65 @@
+# To execute these tests run the following command from the root of the repository: Invoke-Pester -Script .\Scripts\Tests\Write-ReadmeSummarySection.Tests.ps1
+
+# Set a global variable with the path of the executing script
+$global:executingTestPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+BeforeAll {
+    # Import Search-GitHubRepositories function
+    Import-Module $global:executingTestPath/../Write-ReadmeSummarySection.ps1 -Force
+
+    # Import required modules
+    Import-Module $global:executingTestPath/../../Scripts/Write-MarkdownSection.ps1 -Force
+    Import-Module $global:executingTestPath/../../Scripts/New-ShieldIoBadge.ps1 -Force
+    Import-Module $global:executingTestPath/../../Scripts/Write-ReadmeSummarySection.ps1 -Force
+}
+
+Describe "Write-ReadmeSummarySection Unit Tests" {
+    BeforeAll {
+        $GitHubRepositoriesDetails = @(
+            [pscustomobject]@{
+                openedGoodFirstIssues = 0
+                openedHelpWantedIssues = 0
+                isSecurityPolicyEnabled = $true
+            },
+            [pscustomobject]@{
+                openedGoodFirstIssues = 1
+                openedHelpWantedIssues = 0
+                isSecurityPolicyEnabled = $false
+            },
+            [pscustomobject]@{
+                openedGoodFirstIssues = 0
+                openedHelpWantedIssues = 1
+                isSecurityPolicyEnabled = $true
+            },
+            [pscustomobject]@{
+                openedGoodFirstIssues = 1
+                openedHelpWantedIssues = 1
+                isSecurityPolicyEnabled = $false
+            }
+        )
+
+        $Topics = @(
+            [pscustomobject]@{
+                topic = "topic1"
+            },
+            [pscustomobject]@{
+                topic = "topic2"
+            }
+        )
+        
+        # Mock Write-MarkdownSection function
+        Mock Write-MarkdownSection {
+            param($Content, $Path)
+
+            # Do nothing
+        }
+    }
+
+    It "Generates the summary section content" {
+        $expectedContent = "<h3 align='center'>`n  <img alt='Repositories Count Badge' src='https://img.shields.io/badge/Repositories-4-violet'>`n  <img alt='Opened Good First Issues Count Badge' src='https://img.shields.io/badge/Good_First_Issues-2-green'>`n  <img alt='Opened Help Wanted Issues Count Badge' src='https://img.shields.io/badge/Help_Wanted_Issues-2-blue'>`n  <img alt='Security Policy Enabled Percentage Badge' src='https://img.shields.io/badge/Security_Policy_Enabled_Percentage-50-orange'>`n</h3>*"
+        $content = Write-ReadmeSummarySection -GitHubRepositoriesDetails $GitHubRepositoriesDetails -Topics $Topics
+        $content | Should -BeLike $expectedContent
+        $content | Should -BeLike "*<p align='center'>`n  <img alt='topic1 Badge' src='https://img.shields.io/badge/topic1-*"
+        $content | Should -BeLike "*'>`n  <img alt='topic2 Badge' src='https://img.shields.io/badge/topic2-*"
+    }
+}
---
+++ Scripts/Write-ReadmeSummarySection.ps1
@@ -0,0 +1,85 @@
+function Write-ReadmeSummarySection {
+    <#
+        .SYNOPSIS
+            Replace the content of the summary section of the README file.
+
+        .DESCRIPTION
+            Replace the content of the summary section of the README file with insights from the GitHub repositories details.
+
+        .PARAMETER GitHubRepositoriesDetails
+            Details of the GitHub Repositories.
+
+        .PARAMETER Topics
+            List of topics considered to search GitHub repositories.
+
+        .INPUTS
+            None. You cannot pipe objects to Write-ReadmeSummarySection.
+
+        .OUTPUTS
+            None. This script return the content of the summary section as a string.
+
+        .EXAMPLE
+            Import-Module .\Scripts\Write-MarkdownSection.ps1 -Force
+            Import-Module .\Scripts\New-ShieldIoBadge.ps1 -Force
+            Import-Module .\Scripts\Write-ReadmeSummarySection.ps1 -Force
+            Write-ReadmeSummarySection -GitHubRepositoriesDetails $GitHubRepositoriesDetails -Topics $Topics
+    #>
+
+    [CmdletBinding()]
+    [OutputType([string])]
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Object[]]$GitHubRepositoriesDetails,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.Object[]]$Topics
+    )
+
+    Process {
+        # Get the number of repositories referenced
+        $repositoriesCount = $GitHubRepositoriesDetails.count
+
+        # Get the number of opened good first issues in the referenced repositories
+        $openedGoodFirstIssuesCount = $GitHubRepositoriesDetails | Where-Object { $_.openedGoodFirstIssues -gt 0 } | Measure-Object | Select-Object -ExpandProperty Count
+
+        # Get the number of opened help wanted issues in the referenced repositories
+        $openedHelpWantedIssuesCount = $GitHubRepositoriesDetails | Where-Object { $_.openedHelpWantedIssues -gt 0 } | Measure-Object | Select-Object -ExpandProperty Count
+
+        # Get the percentage of referenced repositories with security policy enabled
+        $securityPolicyEnabledPercentage = [math]::Round(($GitHubRepositoriesDetails | Where-Object { $_.isSecurityPolicyEnabled -eq $true } | Measure-Object | Select-Object -ExpandProperty Count) / $repositoriesCount * 100)
+
+        # Configure summary badges
+        $summaryBadgesCentered = "<h3 align='center'>`n"
+
+        $summaryBadgesCentered += "  " + (New-ShieldIoBadge -AlternativeText "Repositories Count Badge" -Message "Repositories" -Label $repositoriesCount -Color "violet" -OutputFormat "HTML") + "`n"
+        $summaryBadgesCentered += "  " + (New-ShieldIoBadge -AlternativeText "Opened Good First Issues Count Badge" -Message "Good First Issues" -Label $openedGoodFirstIssuesCount -Color "green" -OutputFormat "HTML") + "`n"
+        $summaryBadgesCentered += "  " + (New-ShieldIoBadge -AlternativeText "Opened Help Wanted Issues Count Badge" -Message "Help Wanted Issues" -Label $openedHelpWantedIssuesCount -Color "blue" -OutputFormat "HTML") + "`n"
+        $summaryBadgesCentered += "  " + (New-ShieldIoBadge -AlternativeText "Security Policy Enabled Percentage Badge" -Message "Security Policy Enabled Percentage" -Label $securityPolicyEnabledPercentage -Color "orange" -OutputFormat "HTML") + "`n"
+
+        $summaryBadgesCentered += "</h3>`n`n"
+
+        # Concatenate the topics property in the configuration file as static badges
+        foreach ($topic in $Topics)
+        {
+          $topicName = $topic.topic
+          
+          $topicBadge = "  " + (New-ShieldIoBadge -AlternativeText "$topicName Badge" -Message $topicName -OutputFormat "HTML") + "`n"
+          
+          $listOfTopics += $topicBadge
+        }
+
+        # Center the list of topics
+        $listOfTopicsCentered = "<p align='center'>`n"
+        $listOfTopicsCentered += "$listOfTopics"
+        $listOfTopicsCentered += "</p>"
+
+        # Summary content composed of the summary badges and the list of topics
+        $summaryContent = $summaryBadgesCentered + $listOfTopicsCentered
+
+        Write-MarkdownSection -MarkdownFilePath ".\README.md" -SectionIdentifier "summary" -SectionContent $summaryContent
+
+        $summaryContent
+    }
+}
