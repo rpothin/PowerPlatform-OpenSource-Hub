2dd1689501ee7083d56cfeec0306c3c36130b3d7
Raphael Pothin <raphael.pothin@gmail.com>
2023-11-13 19:46:39 +0000

    Add count values to filters + Some spaces in filter pane

--- Website/src/components/FilterPane/index.tsx
+++ Website/src/components/FilterPane/index.tsx
@@ -3,26 +3,64 @@ import React, { useState } from 'react';
 import { Stack, Text, Checkbox, DefaultButton, useTheme } from '@fluentui/react';
 import styles from './styles.module.css';
 
-// Function to extract distinct topics from items
-function extractDistinctTopics(items: { topics?: string[] }[]): string[] {
-  const topicsSet = new Set<string>();
-  items.forEach(item => {
-    if (item.topics) {
-      item.topics.forEach(topic => topicsSet.add(topic));
-    }
-  });
-  return Array.from(topicsSet).sort();
+// Function to count the number of items where the 'hasGoodFirstIssues' property is true
+function countGoodFirstIssues(items: { hasGoodFirstIssues?: boolean }[]): number {
+  return items.filter(item => item.hasGoodFirstIssues).length;
 }
 
-// Function to extract distinct languages from items
-function extractDistinctLanguages(items: { language?: string }[]): string[] {
-  const languagesSet = new Set<string>();
+// Function to count the number of items where the 'hasHelpWantedIssues' property is true
+function countHelpWantedIssues(items: { hasHelpWantedIssues?: boolean }[]): number {
+  return items.filter(item => item.hasHelpWantedIssues).length;
+}
+
+// Function to extract distinct topics from items ordered by count of items with that topic
+function extractDistinctTopics(items: { topics?: string[] }[]): string[] {
+  const topicCounts: { [topic: string]: number } = {};
   items.forEach(item => {
-    if (item.language) {
-      languagesSet.add(item.language);
+    if (item.topics) {
+      item.topics.forEach(topic => {
+        if (!topicCounts[topic]) {
+          topicCounts[topic] = 0;
+        }
+        topicCounts[topic]++;
+      });
     }
   });
-  return Array.from(languagesSet).sort();
+
+  const sortedTopics = Object.entries(topicCounts)
+    .sort((a, b) => b[1] - a[1])
+    .map(([topic]) => topic);
+
+  return sortedTopics;
+}
+
+// Function to count the number of items with a specified topic
+function countItemsWithTopic(items: { topics?: string[] }[], topic: string): number {
+  return items.filter(item => item.topics && item.topics.includes(topic)).length;
+}
+
+// Function to extract distinct languages from items ordered by count of items with that language
+function extractDistinctLanguages(items: { language?: string }[]): string[] {
+  const languageCounts: { [language: string]: number } = {};
+  items.forEach(item => {
+    if (item.language) {
+      if (!languageCounts[item.language]) {
+        languageCounts[item.language] = 0;
+      }
+      languageCounts[item.language]++;
+    }
+  });
+
+  const sortedLanguages = Object.entries(languageCounts)
+    .sort((a, b) => b[1] - a[1])
+    .map(([language]) => language);
+
+  return sortedLanguages;
+}
+
+// Function to count the number of items with a specified language
+function countItemsWithLanguage(items: { language?: string }[], language: string): number {
+  return items.filter(item => item.language === language).length;
 }
 
 // Defining the FilterPane component
@@ -42,6 +80,10 @@ const FilterPane = ({ items, onGoodFirstIssueChange, onHelpWanteIssueChange, onT
   // Extracting topics and languages from items
   const topics = extractDistinctTopics(items);
   const languages = extractDistinctLanguages(items);
+
+  // Counts
+  const goodFirstIssueCount = countGoodFirstIssues(items);
+  const helpWantedIssueCount = countHelpWantedIssues(items);
 
   // Determining which topics and languages to display
   const displayedTopics = showAllTopics ? topics : topics.slice(0, 10);
@@ -81,9 +123,17 @@ const FilterPane = ({ items, onGoodFirstIssueChange, onHelpWanteIssueChange, onT
   return (
   <div className={styles.filterPane}> 
     <Stack className="filter-pane" tokens={{ childrenGap: 10 }}>
-      <Text variant="large" style={{ color: isDarkTheme ? 'rgb(173, 173, 173)' : 'inherit' }}>Help Required</Text>
+      <Text 
+        variant="large" 
+        style={{ 
+          color: isDarkTheme ? 'rgb(173, 173, 173)' : 'inherit', 
+          marginBottom: '15px' 
+        }}
+      >
+        Help Required
+      </Text>
       <Checkbox 
-        label="Has good first issue" 
+        label={"Has good first issue (" + goodFirstIssueCount +  ")"} 
         onChange={(e, checked) => handleGoodFirstIssueChange(checked)}
         styles={{ 
           root: {
@@ -105,7 +155,7 @@ const FilterPane = ({ items, onGoodFirstIssueChange, onHelpWanteIssueChange, onT
         }}
       />
       <Checkbox 
-        label="Has help wanted issue" 
+        label={"Has help wanted issue (" + helpWantedIssueCount + ")"} 
         onChange={(e, checked) => handleHelpWantedIssueChange(checked)}
         styles={{ 
           root: {
@@ -126,13 +176,22 @@ const FilterPane = ({ items, onGoodFirstIssueChange, onHelpWanteIssueChange, onT
           },
         }}
       />
-      <Text variant="large" style={{ color: isDarkTheme ? 'rgb(173, 173, 173)' : 'inherit' }}>Topics</Text>
+      <Text 
+        variant="large" 
+        style={{ 
+          color: isDarkTheme ? 'rgb(173, 173, 173)' : 'inherit', 
+          marginTop: '20px', 
+          marginBottom: '15px' 
+        }}
+      >
+        Topics
+      </Text>
       <Stack tokens={{ childrenGap: 10 }}>
         <Stack>
           {displayedTopics.map((topic, index) => (
             <Checkbox 
               key={index} 
-              label={topic} 
+              label={topic + " (" + countItemsWithTopic(items, topic) + ")"} 
               onChange={(e, checked) => handleTopicChange(topic, checked)}
               styles={{ 
                 root: {
@@ -162,13 +221,22 @@ const FilterPane = ({ items, onGoodFirstIssueChange, onHelpWanteIssueChange, onT
           </DefaultButton>
         )}
       </Stack>
-      <Text variant="large" style={{ color: isDarkTheme ? 'rgb(173, 173, 173)' : 'inherit' }}>Languages</Text>
+      <Text 
+        variant="large" 
+        style={{ 
+          color: isDarkTheme ? 'rgb(173, 173, 173)' : 'inherit', 
+          marginTop: '20px', 
+          marginBottom: '15px' 
+        }}
+      >
+        Languages
+      </Text>
       <Stack tokens={{ childrenGap: 10 }}>
         <Stack>
           {displayedLanguages.map((language, index) => (
             <Checkbox 
               key={index} 
-              label={language} 
+              label={language + " (" + countItemsWithLanguage(items, language) + ")"} 
               onChange={(e, checked) => handleLanguageChange(language, checked)}
               styles={{ 
                 root: {
