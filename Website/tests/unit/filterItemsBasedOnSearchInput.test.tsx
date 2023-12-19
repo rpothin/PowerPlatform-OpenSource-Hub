f772797103a7c477716a7fcbdb30bded1b4bf46e
Raphael Pothin <raphael.pothin@gmail.com>
2023-12-19 02:14:53 +0000

    Update unit tests for filterItemsBasedOnSearchInput

--- Website/tests/unit/filterItemsBasedOnSearchInput.test.tsx
+++ Website/tests/unit/filterItemsBasedOnSearchInput.test.tsx
@@ -8,16 +8,16 @@ const data: Repository[] = [
     url: 'https://github.com/user1/repo1',
     name: 'repo1',
     owner: {
-      login: 'user1',
+      login: 'microsoft',
     },
-    description: 'This is Repo 1',
+    description: 'This is an awesome repository!',
     license: {
       name: 'MIT',
     },
     codeOfConduct: {
       name: 'Contributor Covenant',
     },
-    topics: ['topic1', 'topic2'],
+    topics: ['powerapps', 'powerautomate'],
     language: 'JavaScript',
     stargazerCount: 100,
     watchers: {
@@ -36,7 +36,7 @@ const data: Repository[] = [
     url: 'https://github.com/user2/repo2',
     name: 'repo2',
     owner: {
-      login: 'user2',
+      login: 'rpothin',
     },
     description: 'This is Repo 2',
     license: {
@@ -45,7 +45,7 @@ const data: Repository[] = [
     codeOfConduct: {
       name: 'Citizen Code of Conduct',
     },
-    topics: ['topic3', 'topic4'],
+    topics: ['azure', 'cloud'],
     language: 'TypeScript',
     stargazerCount: 200,
     watchers: {
@@ -73,7 +73,47 @@ describe('filterItemsBasedOnSearchInput', () => {
     expect(result).toEqual([data[1]]);
   });
 
-  it('return a filtered list when the search text is not empty and the search text has a fuzzy match in the fullname of a repository', () => {
+  it('returns a filtered list when the search text is not empty and the search text has a fuzzy match in the fullname of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'rppooothin');
+    expect(result).toEqual([data[1]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has an exact match in the description of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'awesome');
+    expect(result).toEqual([data[0]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has a fuzzy match in the description of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'awesom');
+    expect(result).toEqual([data[0]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has an exact match in the topics of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'powerapps');
+    expect(result).toEqual([data[0]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has a fuzzy match in the topics of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'powerautomt');
+    expect(result).toEqual([data[0]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has an exact match in the language of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'JavaScript');
+    expect(result).toEqual([data[0]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has a fuzzy match in the language of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'JavScrpt');
+    expect(result).toEqual([data[0]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has an exact match in the owner of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'rpothin');
+    expect(result).toEqual([data[1]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has a fuzzy match in the owner of a repository', () => {
     const result = filterItemsBasedOnSearchInput(data, 'rppooothin');
     expect(result).toEqual([data[1]]);
   });
@@ -83,8 +123,23 @@ describe('filterItemsBasedOnSearchInput', () => {
     expect(result).toEqual([data[0]]);
   });
 
+  it('returns a filtered list when the search text is not empty and the search text has a fuzzy match in the license name of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'MIIT');
+    expect(result).toEqual([data[0]]);
+  });
+
+  it('returns a filtered list when the search text is not empty and the search text has an exact match in the code of conduct name of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'Citizen Code of Conduct');
+    expect(result).toEqual([data[1]]);
+  });
+
   it('returns a filtered list when the search text is not empty and the search text has a fuzzy match in the code of conduct name of a repository', () => {
     const result = filterItemsBasedOnSearchInput(data, 'Covenaant');
     expect(result).toEqual([data[0]]);
+  });
+
+  it('returns an empty list when the search text is not empty and the search text does not match any of the fields of a repository', () => {
+    const result = filterItemsBasedOnSearchInput(data, 'abc');
+    expect(result).toEqual([]);
   });
 });
