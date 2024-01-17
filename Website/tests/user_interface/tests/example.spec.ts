20c6cf70acd8dde64b8591b0079790fb34f7c036
Raphael Pothin <raphael.pothin@gmail.com>
2024-01-17 04:49:55 +0000

    Changes to improve the testing experience with Playwright locally

--- Website/tests/user_interface/tests/example.spec.ts
+++ Website/tests/user_interface/tests/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-// Validate that the title of the "http://localhost:3000/PowerPlatform-OpenSource-Hub/" web page is "Power Platform Open-Source Hub"
+// Validate that the title of the website is "Power Platform Open-Source Hub"
 // The class of the title is "hero__title"
 test('Validate that the title of the website opened locally', async ({ page }) => {
-  await page.goto('http://localhost:3000/PowerPlatform-OpenSource-Hub/');
+  await page.goto('/');
   await expect(page).toHaveTitle(/Power Platform Open-Source Hub/);
 });
